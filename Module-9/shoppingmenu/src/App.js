8caf26323365c0055fbd0c56d30dfccabb92b505
purwang28 <purvang2810@gmail.com>
2023-05-01 22:38:32 +0530

    .

---
+++ Module-9/shoppingmenu/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import TopNavBar from './component/TopNavBar';
+import Navbar from './component/Navbar';
+import Content from './component/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar/>
+      <Navbar/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
